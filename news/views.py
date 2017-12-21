43203986ef3ed17fa5f0a7ac7e270bfa6e080bf9
rybas-dv <dirybas@rambler.ru>
2017-12-21 13:53:02 +0300

    ver 1.1

--- news/views.py
+++ news/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
-# def home(request):
-#     return render(request, 'prof/home.html')
-#
-# def documents(request):
-#     return render(request, 'prof/documents.html')
-#
-# def base(request):
-#     return render(request, 'prof/base.html')
+def home(request):
+    return render(request, 'prof/home.html')
+
+def documents(request):
+    news = Konkurs.objects.all()
+    context = {
+        'news': news
+    }
+    return render(request, 'prof/documents.html', context)
+
+def base(request):
+    return render(request, 'prof/base.html') 	
+ 	
